b4d097bbc935b2f3b02ceace26309022c57907b6
Miriam <miriam.haenle@gmail.com>
2020-12-02 10:06:37 +0100

    Setup mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
 import express from 'express';
 import path from 'path';
 import { dirname } from './helpers.js';
+import { default as mongodb } from 'mongodb';
+let MongoClient = mongodb.MongoClient;
 
 const app = express();
 const __dirname = dirname(import.meta.url);
+const databaseUser = 'MathewGriffinthore';
+const databasePassword = 'A1hrmRBi3QyfRDGB';
+const databaseName = 'harry-potter-wizard-game';
+
+const uri = `mongodb+srv://${databaseUser}:${databasePassword}@miriam-super-cluster.q4ahs.mongodb.net/test?authSource=admin&replicaSet=atlas-pq5i4z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
+
+async function run() {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection('hogwarts-houses');
+    const housesPoints = await collection
+      .find({})
+      .toArray((error, docs) => console.log(docs));
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -13,6 +35,11 @@ app.get('/', (req, res) => {
 
 app.get('/api', (req, res) => {
   res.json({ status: 'Alive and awake' });
+});
+
+app.get('/dbs', (req, res) => {
+  run();
+  res.send('Mongo db works');
 });
 
 const PORT = process.env.PORT || 4000;
